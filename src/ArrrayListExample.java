d042e7748a605aa65bb66cfec611c6a2d173214d
mmhayman <mmhayman@svsu.edu>
2017-07-10 10:13:11 -0400

    switched around

--- src/ArrrayListExample.java
+++ src/ArrrayListExample.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
 /**
@@ -20,49 +20,20 @@ public class ArrrayListExample {
 
             String userInput = " ";
 
-            // array list
-
-            ArrayList<Movies> movieList = new ArrayList<Movies>();
-
-            // array list with movie titles and category
-
-            movieList.add(new Movies("The Boss Baby", "Animated"));
-            movieList.add(new Movies("Cars 3", "Animated"));
-            movieList.add(new Movies("Live by Night", "Drama"));
-            movieList.add(new Movies("Hidden Figures", "Drama"));
-            movieList.add(new Movies("Gifted", "Drama"));
-            movieList.add(new Movies("Get Out", "Horror"));
-            movieList.add(new Movies("It Comes at Night", "Horror"));
-            movieList.add(new Movies("Flatliners", "Horror"));
-            movieList.add(new Movies("Wonder Woman", "Scifi"));
-            movieList.add(new Movies("Logan", "Scifi"));
 
 
             System.out.print("What category are you interested in? (animated, horror, drama, or scifi)");
             userInput = scan.nextLine();
             userInput.toLowerCase();
 
-            // if user writes any of these category names - it will list the movies in that category
-            if (userInput.equalsIgnoreCase("animated")) {
-                System.out.println(movieList.get(0));
-                System.out.println(movieList.get(1));
+            Movies moviesList = new Movies(userInput);
 
+            // loop to get movies
+            for (Movies m: moviesList.listofMovies()
+                 ) {
+            if (userInput.equalsIgnoreCase(m.getCategory())) {
+                System.out.println(m);
             }
-            if (userInput.equalsIgnoreCase("drama")) {
-                System.out.println(movieList.get(2));
-                System.out.println(movieList.get(3));
-                System.out.println(movieList.get(4));
-            }
-
-            if (userInput.equalsIgnoreCase("horror")) {
-                System.out.println(movieList.get(5));
-                System.out.println(movieList.get(6));
-                System.out.println(movieList.get(7));
-            }
-
-            if (userInput.equalsIgnoreCase("scifi")) {
-                System.out.println(movieList.get(8));
-                System.out.println(movieList.get(9));
             }
 
             System.out.println();
@@ -78,4 +49,3 @@ public class ArrrayListExample {
 
 
 }
-
--- src/Movies.java
+++ src/Movies.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created by michelhayman on 7/9/17.
  */
@@ -5,6 +7,10 @@ public class Movies {
 
     private String title;
     private String category;
+
+    public Movies(String userInput) {
+
+    }
 
     public String getTitle() {
         return title;
@@ -27,9 +33,29 @@ public class Movies {
         this.category = category;
     }
 
-@Override
+    public ArrayList<Movies> listofMovies () {
+
+        ArrayList<Movies> movieList = new ArrayList<>();
+
+        // array list with movie titles and category
+
+        movieList.add(new Movies("The Boss Baby", "Animated"));
+        movieList.add(new Movies("Cars 3", "Animated"));
+        movieList.add(new Movies("Live by Night", "Drama"));
+        movieList.add(new Movies("Hidden Figures", "Drama"));
+        movieList.add(new Movies("Gifted", "Drama"));
+        movieList.add(new Movies("Get Out", "Horror"));
+        movieList.add(new Movies("It Comes at Night", "Horror"));
+        movieList.add(new Movies("Flatliners", "Horror"));
+        movieList.add(new Movies("Wonder Woman", "Scifi"));
+        movieList.add(new Movies("Logan", "Scifi"));
+
+        return movieList;
+    }
+
+    @Override
     public String toString () {
         return title;
-}
+    }
 
 }
